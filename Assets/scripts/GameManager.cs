7809d816ca9179679981acb91c170f5715bbaeda
Harman <hkhi70381@gmail.com>
2018-09-24 17:57:26 -0700

    my first commit

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public delegate void Gamedelegate();
+    public static event Gamedelegate OnGameStarted;
+    public static event Gamedelegate OnGameOverConfirmed;
+
+    public static GameManager Instance;
+
+    public GameObject startPage;
+    public GameObject gameOverPage;
+    public GameObject countdownpage;
+    public Text scoretext;
+
+    enum PageState {
+        None,
+        Start,
+        GameOver,
+        Countdown
+    }
+
+    int score = 0;
+    bool gameOver = false;
+
+    public bool Gameover { get { return gameOver; } }
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    void SetPageState(PageState state) {
+        switch (state) {
+            case PageState.None:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                countdownpage.SetActive(false);
+                break;
+            case PageState.Start:
+                startPage.SetActive(true);
+                gameOverPage.SetActive(false);
+                countdownpage.SetActive(false);
+                break;
+            case PageState.GameOver:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(true);
+                countdownpage.SetActive(false);
+                break;
+            case PageState.Countdown:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                countdownpage.SetActive(true);
+                break;
+        }
+    }
+
+    public void ConfirmGameOver() {
+        //activated when replay button is hit
+    }
+
+    public void StartGame() {
+        //activated when play button is hit
+    }
+}
---
+++ Assets/scripts/TapController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TapController : MonoBehaviour {
+
+    public float tapForce = 10;
+    public float tiltsmooth = 5;
+    public Vector3 startPos;
+    public float myfloat;
+    Rigidbody2D rigidbody;
+    Quaternion downRotation;
+    Quaternion fowardRotation;
+
+    void Start() {
+        rigidbody = GetComponent<Rigidbody2D>();
+        downRotation = Quaternion.Euler(0, 0, -70);
+        fowardRotation = Quaternion.Euler(0, 0, 35);
+    
+    }
+
+    void Update(){
+        if (Input.GetMouseButtonDown(0)) {
+            transform.rotation = fowardRotation;
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltsmooth * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.tag == "ScoreZone") {
+            //register a score event
+            //play a sound
+        }
+
+        if (col.gameObject.tag == "DeadZone") {
+            rigidbody.simulated = false;
+            //register a dead event
+            //play a sound
+        }
+    }
+}
